81580b9f0f52dbc6b650aaf33f6a9fb05a89b593
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-13 20:24:59 +0530

    code is added

--- loop.cpp
+++ loop.cpp
@@ -4,12 +4,21 @@ using namespace std;
 int main()
 {
  
-    int n,i;
-    cout<<"enter the number of rows: ";
-    cin>>n;
-    for(i=n;i>=1;i--)
-    {
-        cout<<i<<endl;
-    }
-    return 0;
+    // int n,i;
+    // cout<<"enter the number of rows: ";
+    // cin>>n;
+    // for(i=n;i>=1;i--)
+    // {
+    //     cout<<i<<endl;
+    // }
+    // return 0;
+
+char name;
+
+for(name='a';name<='z';name++)
+{
+    cout<<name<<endl;
 }
+}
+
+
