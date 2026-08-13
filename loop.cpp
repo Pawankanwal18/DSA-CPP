ba7006d4d04b945625781523b59bd95d7adfa0fd
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-13 21:53:40 +0530

    new code is added

--- loop.cpp
+++ loop.cpp
@@ -46,12 +46,22 @@ int main()
 //     cout<<n*i<<endl;
 // }
 
-int i,sum=0,n;
-cout<<"enter the number: ";
-cin>>n;
-for(i=1;i<=n;i++)
-{
-    sum= sum+i;
-}
-cout<<"sum: "<<sum<<endl;
+// int i,sum=0,n;
+// cout<<"enter the number: ";
+// cin>>n;
+// for(i=1;i<=n;i++)
+// {
+//     sum= sum+i;
+// }
+// cout<<"sum: "<<sum<<endl;
+ int i,fact=1,n;
+ cout<<"Enter the number: ";
+ cin>>n;
+ for(i=1;i<=n;i++)
+ {
+    fact =fact*i;
+ }
+cout<<"factorial:" <<fact<<endl;
+
+
 }
