a83e1bee4c229dfc6af574fdfebddb40466327ff
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-14 09:01:36 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -3,13 +3,44 @@ using namespace std;
 
 int main(){
     int i,j;
-    for(j=1;j<=5;j++)
-    {
+    // for(j=1;j<=5;j++)
+    // {
 
-        for(i=1;i<=5;i++)
-        {
-            cout<<"* "<<" ";
-        }
-        cout<<endl;
+    //     for(i=1;i<=5;i++)
+    //     {
+    //         cout<<"* "<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+// for(j=1;j<=5;j++)
+// {
+//     for(i=1;i<=5;i++)
+//     {
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }        
+
+
+// for(j=1;j<=5;j++)
+// {
+//     for(i=5;i>=1;i--)
+//     {
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }        
+
+for(j=1;j<=5;j++)
+{
+    for(i=1;i<=5;i++)
+    {
+        cout<<i*i<<" ";
     }
+    cout<<endl;
+}        
+
+
 }
+
+
