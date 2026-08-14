0f0e4571862e173dde6ad82da670ecc7a3e7fcfd
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-14 22:51:23 +0530

    added

--- mediumpattern.cpp
+++ mediumpattern.cpp
@@ -25,15 +25,31 @@ int row,col;
 //     cout<<endl;
 // }
 
+// for(row=1;row<=5;row++)
+// {
+//     for(col=row;col>=1;col--)
+//     {
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// for(row=1;row<=5;row++)
+// {
+//     for(col=1;col<=5-(row-1);col++)
+//     {
+//         cout<<"*"<<" ";
+//     }
+//     cout<<endl;
+// }
+
 for(row=1;row<=5;row++)
 {
-    for(col=row;col>=1;col--)
+    for(col=1;col<=5-(row-1);col++)
     {
         cout<<col<<" ";
     }
     cout<<endl;
 }
-
-
 
 }
