e7d051fdb67dc1027c65473666fffb1fe2d50ad5
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-14 22:00:17 +0530

    code is added

--- mediumpattern.cpp
+++ mediumpattern.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main()
 {
-int i,j;
+// int i,j;
+// for(j=1;j<=5;j++)
+// {
+    
+//     for(i= 1;i<=j;i++)
+//     {
+//         cout<<"*" <<" ";
+//     }
+//     cout<<endl;
+// }
+
+int row,col;
+
+for(row=1;row<=5;row++)
+{
+    for(col=1;col<=row;col++)
+    {
+        cout<<col<<" ";
+    }
+    cout<<endl;
+}
 }
