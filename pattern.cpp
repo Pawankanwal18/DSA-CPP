3b1e664a91ee8a774c14768eee9533ea88fa3fde
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-14 21:36:27 +0530

    added

--- pattern.cpp
+++ pattern.cpp
@@ -31,16 +31,50 @@ int main(){
 //     cout<<endl;
 // }        
 
+// for(j=1;j<=5;j++)
+// {
+//     for(i=1;i<=5;i++)
+//     {
+//         cout<<i*i<<" ";
+//     }
+//     cout<<endl;
+// }        
+
+// for(j=1;j<=5;j++)
+// {
+//     char name ='a'+j-1;
+//     for(i=1;i<=5;i++)
+//     {
+//         cout<<name<<" ";
+//     }
+//     cout<<endl;
+// }        
+
+
+// }
+
+// for(j=1;j<=5;j++)
+// {
+   
+//     for(i=1;i<=5;i++)
+//     {
+//          char name ='a'+i-1;
+//         cout<<name<<" ";
+//     }
+//     cout<<endl;
+// }        
+
+int count=1;
+ 
 for(j=1;j<=5;j++)
 {
+   
     for(i=1;i<=5;i++)
     {
-        cout<<i*i<<" ";
+        cout<<count<<" ";
+        count++;
     }
     cout<<endl;
-}        
-
-
+}   
 }
-
 
