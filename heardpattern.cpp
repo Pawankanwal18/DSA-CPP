3c44e3096803a2fa961959346a752c6bc4350e2b
Pawan Kanwal <pawanbhai@gmail.com>
2026-08-15 22:53:39 +0530

    code is written

--- heardpattern.cpp
+++ heardpattern.cpp
@@ -3,17 +3,44 @@ using namespace std;
 int main()
 {
      int row , col;
-     for(row =1; row<=5;row++)
+    //  for(row =1; row<=5;row++)
+    //  {
+
+    //     for(col=1;col<=5-row;col++)
+    //     {
+
+    //         cout<<"  ";
+    //     }
+
+    //         for(col=1;col<=row;col++)
+    //         {
+                
+    //             cout<<row<<" "; 
+            
+    //         }
+    //         cout<<endl;
+    //     }
+    
+  for(row =1; row<=5;row++)
      {
+
         for(col=1;col<=5-row;col++)
         {
-            cout<<" ";
+
+            cout<<"  ";
         }
+
             for(col=1;col<=row;col++)
             {
-                cout<<"* ";
+                
+                cout<<row<<" "; 
             
             }
             cout<<endl;
         }
-     }
+
+
+
+
+
+}
