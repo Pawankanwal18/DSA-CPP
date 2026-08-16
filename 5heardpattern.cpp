9392ab4d65cfb7c549150caa3ea696c7071ce1b5
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-16 08:10:09 +0530

    code is aded

--- 5heardpattern.cpp
+++ 5heardpattern.cpp
@@ -22,20 +22,20 @@ int main()
     // }
    
 
-cout<<"enter the input ";
-cin>>n;
+// cout<<"enter the input ";
+// cin>>n;
 
-for(row=1;row<=n;row++)
-{
-    for(col=1;col<=n-row;col++)
-    cout<<" ";
+// for(row=1;row<=n;row++)
+// {
+//     for(col=1;col<=n-row;col++)
+//     cout<<" ";
+    
+//     for(col=1;col<=row;col++)
+//     cout<<col<<" ";
 
-    for(col=1;col<=row;col++)
-    cout<<col<<" ";
+//     for(col=row-1;col>=1;col--)
+//     cout<<col<<" ";
 
-    for(col=row-1;col>=1;col--)
-    cout<<col<<" ";
-
-    cout<<endl;
-}
+//     cout<<endl;
+// }
 }
