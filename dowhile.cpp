4e2cf554c57b5355a6500d090a734ded90ca670f
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-18 08:56:46 +0530

    new codea added and code optimized

--- dowhile.cpp
+++ dowhile.cpp
@@ -1,17 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int i=1;
+    
     int n;
+    int sum=0;
    cout<<"Enter the inpute";
    cin>>n;
-  i=1;
-  do{
-    cout<<i<<endl;
+//   i=1;
+//   do{
+//     cout<<i<<endl;
 
+//     i++;
+//   } 
+//   while(i<=n) ;
+
+
+ int i=1;
+ do{  
+    sum =sum+i;
+   
     i++;
-  } 
-  while(i<=n) ;
+ }
+while(i<=n);
+
+ cout<<sum<<endl;
 }
