b77ea0f6ad15a30ada6fbb42f7a83351980a1352
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-18 07:52:23 +0530

    code is created

--- whiledoloop.cpp
+++ whiledoloop.cpp
@@ -1,2 +1,21 @@
   
-  
+  //print 1 to 10
+
+  #include<iostream>
+  using namespace std;
+
+  int main(){
+
+    int i;
+
+    i=1;
+    while(i<=10)
+    {
+        cout<<i<<" ";
+        i++;
+        cout<<endl;
+    }
+
+  }
+
+
