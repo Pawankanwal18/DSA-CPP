5b1c4943bd5c95bbc5db875087a3d8b6313ce67a
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-18 09:02:13 +0530

    code added

---
+++ break&continue.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter the input";
+    cin>>n;
+
+
+    int i=1;
+    while(i<=n)
+    {
+        if(i==4)
+        break;
+        cout<<i<<" ";
+        i++;
+    }
+}
