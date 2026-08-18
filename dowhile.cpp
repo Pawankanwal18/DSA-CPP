e6732c060aaf95027a0ebb2a49c96bc0b43d1490
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-18 08:45:28 +0530

    correct code is added

---
+++ dowhile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=1;
+    int n;
+   cout<<"Enter the inpute";
+   cin>>n;
+  i=1;
+  do{
+    cout<<i<<endl;
+
+    i++;
+  } 
+  while(i<=n) ;
+}
