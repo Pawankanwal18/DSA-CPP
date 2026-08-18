847aa104b13735516c282e13de7dc045ebc0973e
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-18 08:38:59 +0530

    code is aded

--- whiledoloop.cpp
+++ whiledoloop.cpp
@@ -1,12 +1,12 @@
   
-  print 1 to 10
+//   print 1 to 10
 
   #include<iostream>
   using namespace std;
 
   int main(){
 
-    int i;
+    int i=1 ,n;
 
     // i=1;
     // while(i<=10)
@@ -15,9 +15,50 @@
     //     i++;
     //     cout<<endl;
     // }
-
     
+    cout<<"Enter the inpute ";
+    cin>>n;
+   
+    // while(i<=10)
+    // {
+    //     cout <<n*i<<" ";
+    //     i++;
+    //     cout<<endl;
+    // }
+     
+//    while(i<=n)
+//    {
+   
+//     if(n%i==0)
+//     {
+//         cout<<i;
+//     }
+//         i++;
+//         cout<<endl;
+//     }
+    
+//    }
 
+
+// while(i<=n)
+// {
+
+//     if(i%2==0)
+//     {
+//     cout<<i<<endl;
+//     }
+//     i++;
+// }
+
+  while(i<=n)
+  {
+    if(i % 2 != 0)
+    {
+    cout<<i<<endl;
+    }
+    i++;
   }
+  }
+  
 
 
