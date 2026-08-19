22b810f98de49515e6b98beef59b28630e139f36
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-19 07:44:49 +0530

    code is comment out

--- breakcontinue.cpp
+++ breakcontinue.cpp
@@ -6,21 +6,21 @@ int main() {
 
 int i=1;
 
-for(i=1;i<=10;i++)
-{
-    if(i==4)
-    break;
+// for(i=1;i<=10;i++)
+// {
+//     if(i==4)
+//     break;
     
-    cout<<i<<" ";
-}
-}
-
-//  for(i=1;i<=10;i++)
-//  {
-//  if(i%4 ==0)
-//  continue;
-
-//  cout<<i<<" "; 
-
-//  }
+//     cout<<i<<" ";
 // }
+// }
+
+ for(i=1;i<=10;i++)
+ {
+ if(i%4 ==0)
+ continue;
+
+ cout<<i<<" "; 
+
+ }
+}
