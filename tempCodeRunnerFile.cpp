db253cd8c5921c1a9659fd6cb2aea5783c56432d
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-19 07:31:45 +0530

    added

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,15 @@
-int i ,n;
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+    for(j=1;j<=5;j++)
+    {
+
+        for(i=1;i<=5;i++)
+        {
+            cout<<"* "<<" ";
+        }
+        cout<<endl;
+    }
+}
