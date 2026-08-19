aa6093a729ba5d4e16352fe71c691b0b477baa15
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-19 07:44:18 +0530

    code is addded

---
+++ breakcontinue.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int i=1;
+
+for(i=1;i<=10;i++)
+{
+    if(i==4)
+    break;
+    
+    cout<<i<<" ";
+}
+}
+
+//  for(i=1;i<=10;i++)
+//  {
+//  if(i%4 ==0)
+//  continue;
+
+//  cout<<i<<" "; 
+
+//  }
+// }
